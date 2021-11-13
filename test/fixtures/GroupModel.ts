9f4ac5e913f5bd0f42aff0c99f41df7028c140a2
Yslan Ramos <yslanramos.r@gmail.com>
2021-11-13 04:07:19 -0300

    test: setup mongodb memory

---
+++ test/fixtures/GroupModel.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Model, Types } from 'mongoose';
+
+const { ObjectId } = mongoose.Schema.Types;
+
+export const GroupSchema = new mongoose.Schema(
+  {
+    name: String,
+    users: [
+      {
+        type: ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    collection: 'Group',
+  },
+);
+
+export interface IGroup extends Document {
+  name: string;
+  users: Types.ObjectId[];
+}
+
+const GroupModel: Model<IGroup> = mongoose.model('Group', GroupSchema);
+
+export default GroupModel;
